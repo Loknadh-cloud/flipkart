16102727654bbaf5a471b409306be41785575f76
loknadh_cloud <loknadh4b9@gmail.com>
2021-06-05 12:42:52 +0530

    commiting devops,sh

---
+++ devops.sh
@@ -0,0 +1,18 @@
+hello
+
+welcome.....
+
+this is the first filr i have created in git .....
+
+git commands are so easy and confusing also
+
+important conceptd in git ate
+
+git branching
+
+git merging
+git tags
+ git reset
+ git revert
+ 
+ 
