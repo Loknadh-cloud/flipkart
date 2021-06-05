698c763d579ee5257d75514aba312f0b4777f293
loknadh_cloud <loknadh4b9@gmail.com>
2021-06-05 12:44:24 +0530

    commitin all files

---
+++ exam/user.java
@@ -0,0 +1,15 @@
+hellooooooooo
+everyone
+
+
+good morning...
+
+1) after installing git in our system, open the git bash and create a dir after creatin direstory change to that directory and intialize a new empty local repo by using git init command.
+
+2) after excuting git init command by default a hidden directory named as .git has created . that direstory consists of filres and directorieswhich were used to reun git vcs.
+
+3) now we can create files and in working are and moved to index area by using git add command later we can commit these files in to local repo by using git commit command .
+
+4) now we have to map the local repo with remote repo by using the command git remote add remote repo url
+
+5) after  mapping we can push the files avalable in lacl repo to remote repo by using git push command
